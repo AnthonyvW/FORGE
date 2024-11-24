8ad03027b00b34f29cb74e26f6971313df37aa47
AnthonyvW <akamvw@gmail.com>
2024-11-23 17:04:46 -0900

    Progress update

---
+++ UI/Button.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Button(object):
+    def __init__(self, functionToCall, x:int, y:int, width:int, height:int):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
+        self.functionToCall = functionToCall
+        self.isHover = False
+
+        self.foreground = pygame.Color(64, 255, 64)
+        self.hoverBackground = pygame.Color(128, 128, 255)
+
+        self.background = pygame.Color(32, 128, 32)
+
+    def CheckButton(self, mouseX:int, mouseY:int, mouseClick: bool):
+        if(mouseX > self.x and mouseX < self.x + self.width and mouseY < self.y + self.height and mouseY > self.y):
+            if(mouseClick):
+                self.functionToCall()
+            
+            self.isHover = True
+            return False
+        else:
+            self.isHover = False
+            return False
+
+    def draw(self, surface):
+        if(self.isHover):
+            pygame.draw.rect(surface, self.hoverBackground, (self.x, self.y, self.width, self.height))
+            pygame.draw.rect(surface, self.foreground, (self.x, self.y, self.width, self.height), 2)
+        else:
+            pygame.draw.rect(surface, self.background, (self.x, self.y, self.width, self.height))
+            pygame.draw.rect(surface, self.foreground, (self.x, self.y, self.width, self.height), 2)
+    
+
+
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import pygame
 import numpy as np
+import pygame
+
+from UI.Button import Button
 
 pygame.init()
-pygame.display.set_caption("OpenCV camera stream on Pygame")
-surface = pygame.display.set_mode([1280,720])
+pygame.display.set_caption("Tree Ring Imaging Machine v2")
+screen = pygame.display.set_mode([1280,720])
 #0 Is the built in camera
 cap = cv2.VideoCapture(0)
 #Gets fps of your camera
@@ -13,30 +16,63 @@ print("fps:", fps)
 #If your camera can achieve 60 fps
 #Else just have this be 1-30 fps
 cap.set(cv2.CAP_PROP_FPS, 60)
+# A clock to limit the frame rate.
+clock = pygame.time.Clock()  
+# Font
+defaultFont = pygame.font.SysFont("Arial", 30)
 
-while True:
-    surface.fill([0,0,0])
+# Draws Text on screen at x, y
+def draw_text(text, _x, _y, color=(255, 255, 255), font=defaultFont):
+    img = font.render(text, True, color)
+    screen.blit(img, (_x, _y))
+
+
+def say():
+    print("Hello")
+
+button = Button(say, 1000, 500, 40, 40)
+
+running = True
+while running:
+    clock.tick(60)
+    # Mouse Position
+    pos = pygame.mouse.get_pos()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            button.CheckButton(pos[0], pos[1], True)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        if event.type == pygame.KEYUP:
+            background_color = pygame.Color(255,0,0)
+            screen.fill(background_color)
+
+    # Update
+
+    button.CheckButton(pos[0], pos[1], False)
 
     success, frame = cap.read()
     if not success:
         break
-
+    # The video uses BGR colors and PyGame needs RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     #for some reasons the frames appeared inverted
     frame = np.fliplr(frame)
     frame = np.rot90(frame)
-
-    # The video uses BGR colors and PyGame needs RGB
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
     surf = pygame.surfarray.make_surface(frame)
 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYUP:
-            background_color = red
-            surface.fill(background_color)
-            pygame.display.update
-            end_time = self.time()
 
-    # Show the PyGame surface!
-    surface.blit(surf, (0,0))
+    # Rendering
+
+    screen.fill([0,0,0])
+
+    button.draw(screen)
+    
+    screen.blit(surf, (0,0))
     pygame.display.flip()
+    pygame.display.update()
+
+pygame.quit()
